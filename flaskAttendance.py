54326b528bdea7a83785ec97ce89aed7247de82f
TeeeradejLo <teeradejlo.2002@gmail.com>
2020-02-18 14:30:19 -1000

    complete the getdata function

--- flaskAttendance.py
+++ flaskAttendance.py
@@ -4,6 +4,32 @@ from time import sleep
 import mysql.connector
 import datetime
 
+def getAttendanceData(mycursor, tableName):
+    mycursor.execute("SELECT * FROM " + tableName)
+    
+    myresult = mycursor.fetchall()
+    tableString = '''
+                     <h1 style="text-align:center">Welcome to Attendance System With Flask</h1><br/>
+                     <table style="width:100%; ", border = "1">
+                          <tr>
+                            <th>Student ID</th>
+                            <th>Class ID</th> 
+                            <th>Date and Time</th>
+                          </tr>
+                    '''
+    for i in range(0, len(myresult)):
+        tableString = tableString + '<tr style="text-align:right">'
+        tableString = tableString + '<td>' + myresult[i][0] + '</td>'
+        tableString = tableString + '<td>' + myresult[i][1] + '</td>'
+        tableString = tableString + '<td>' + myresult[i][2] + '</td>' 
+        tableString = tableString + '</tr>'
+        
+    tableString = tableString + "</table>"
+        
+    return tableString
+
+
+
 app = Flask(__name__, static_url_path = "/stuff", static_folder = "static")
 
 mydb = mysql.connector.connect(
@@ -13,29 +39,11 @@ mydb = mysql.connector.connect(
   database = "AttendanceSystem"
 )
 tableName = "attendance"
+mycursor = mydb.cursor()
       
 @app.route("/")
 def home():
-    
-    myhtml = '''
-                <h1>Welcome to Flask</h1><br/>
-                <table style="width:100%; ", border = "1">
-                  <tr>
-                    <th>Firstname</th>
-                    <th>Lastname</th> 
-                    <th>Age</th>
-                  </tr>
-                  <tr style="text-align:right">
-                    <td>Jill</td>
-                    <td>Smith</td> 
-                    <td>50</td>
-                  </tr>
-                  <tr style="text-align:right">
-                    <td>Eve</td>
-                    <td>Jackson</td> 
-                    <td>94</td>
-                  </tr>
-                </table>'''
+    myhtml = getAttendanceData(mycursor, tableName)
     return myhtml
 
 
