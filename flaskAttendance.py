d9e53e0de35affc044517cae5104b7c32d51e56a
TeeeradejLo <teeradejlo.2002@gmail.com>
2020-02-19 13:47:06 -1000

    add css, change some info heading

--- flaskAttendance.py
+++ flaskAttendance.py
@@ -4,23 +4,68 @@ from time import sleep
 import mysql.connector
 import datetime
 
+def getClassName(mycursor, classid):
+    mycursor.execute("SELECT * FROM classes WHERE id = \"" + str(classid) + "\"")
+    
+    myresult = mycursor.fetchall()
+    return myresult[0][1]
+
+
+def getStudentName(mycursor, id):
+    mycursor.execute("SELECT * FROM students WHERE id = \"" + str(id) + "\"")
+    
+    myresult = mycursor.fetchall()
+    return myresult[0][1] + " " + myresult[0][2]
+
+
 def getAttendanceData(mycursor, tableName):
     mycursor.execute("SELECT * FROM " + tableName)
     
     myresult = mycursor.fetchall()
     tableString = '''
-                     <h1 style="text-align:center">Welcome to Attendance System With Flask</h1><br/>
-                     <table style="width:100%; ", border = "1">
+                     <style>
+                     
+                         body {
+                             background-color:#151515;
+                         }
+                         
+                         h1 {
+                             color: white;
+                             text-align: center;
+                             font-family: Arial, Heveltica, san-serif;
+                         }
+                         
+                         table {
+                             width: 80%;
+                             color: white;
+                             text-align:center;
+                             border: 2px solid white;
+                             border-right: transparent;
+                             border-bottom: transparent;
+                         }
+                         
+                         th, td {
+                             border-right: 2px solid white;
+                             border-bottom: 2px solid white;
+                             height: 50px;
+                         }
+                         
+                     </style>
+                        
+                     <h1>Welcome to Attendance System With Flask</h1><br/>
+                     <table align = "center">
                           <tr>
-                            <th>Student ID</th>
-                            <th>Class ID</th> 
+                            <th width = "20%";>Student ID</th>
+                            <th>Student Name</th>
+                            <th>Class Name</th> 
                             <th>Date and Time</th>
                           </tr>
                     '''
     for i in range(0, len(myresult)):
-        tableString = tableString + '<tr style="text-align:right">'
+        tableString = tableString + '<tr>'
         tableString = tableString + '<td>' + myresult[i][0] + '</td>'
-        tableString = tableString + '<td>' + myresult[i][1] + '</td>'
+        tableString = tableString + '<td>' + getStudentName(mycursor, myresult[i][0]) + '</td>'
+        tableString = tableString + '<td>' + getClassName(mycursor, myresult[i][1]) + '</td>'
         tableString = tableString + '<td>' + myresult[i][2] + '</td>' 
         tableString = tableString + '</tr>'
         
